bf20771e7e6cbb4c4421d436c4b7030b10893cc0
ItsMoloy <moloy21@student.sust.edu>
2025-11-02 18:57:29 +0600

    Create category

---
+++ controllers/categoryControllers.js
@@ -0,0 +1,19 @@
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+
+// Create a new category
+exports.createCategory = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    const category = new Category({
+      name,
+      description
+    });
+
+    const savedCategory = await category.save();
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
