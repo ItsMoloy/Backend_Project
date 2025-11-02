bd729097d333e621ca2811c482ee27bbb2c051aa
ItsMoloy <moloy21@student.sust.edu>
2025-11-02 18:54:21 +0600

    Added the functionality for product code

--- models/Product.js
+++ models/Product.js
@@ -43,4 +43,81 @@ const productSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
+// Function to find the longest strictly increasing substring
+function findLongestIncreasingSubstring(str) {
+  str = str.toLowerCase();
+  let maxLength = 0;
+  let maxSubstrings = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSubstring = str[i];
+    let currentChar = str.charCodeAt(i);
+
+    for (let j = i + 1; j < str.length; j++) {
+      const nextChar = str.charCodeAt(j);
+      if (nextChar > currentChar) {
+        currentSubstring += str[j];
+        currentChar = nextChar;
+      } else {
+        break;
+      }
+    }
+
+    if (currentSubstring.length > maxLength) {
+      maxLength = currentSubstring.length;
+      maxSubstrings = [{ substring: currentSubstring, startIndex: i, endIndex: i + currentSubstring.length - 1 }];
+    } else if (currentSubstring.length === maxLength && maxLength > 0) {
+      maxSubstrings.push({ substring: currentSubstring, startIndex: i, endIndex: i + currentSubstring.length - 1 });
+    }
+  }
+
+  return maxSubstrings;
+}
+
+// Generate product code
+productSchema.pre('save', async function(next) {
+  if (!this.isModified('name')) {
+    return next();
+  }
+
+  try {
+    // Hash the product name
+    const hash = crypto.createHash('md5').update(this.name).digest('hex').substring(0, 6);
+
+    // Find longest increasing substrings
+    const longestSubstrings = findLongestIncreasingSubstring(this.name);
+
+    if (longestSubstrings.length === 0) {
+      this.productCode = `${hash}-0${this.name.charAt(0).toLowerCase()}0`;
+      return next();
+    }
+
+    // Concatenate all substrings of equal max length
+    let combinedSubstring = '';
+    let startIndex = longestSubstrings[0].startIndex;
+    let endIndex = longestSubstrings[0].endIndex;
+
+    longestSubstrings.forEach(item => {
+      combinedSubstring += item.substring;
+      endIndex = Math.max(endIndex, item.endIndex);
+    });
+
+    // Generate the product code
+    this.productCode = `${hash}-${startIndex}${combinedSubstring}${endIndex}`;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Virtual for final price after discount
+productSchema.virtual('finalPrice').get(function() {
+  return this.price - (this.price * this.discount / 100);
+});
+
+// Include virtuals when converting to JSON
+productSchema.set('toJSON', { virtuals: true });
+productSchema.set('toObject', { virtuals: true });
+
 module.exports = mongoose.model('Product', productSchema);
